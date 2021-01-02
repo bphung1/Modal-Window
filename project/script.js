b7880c02b0e37153a6b6613e0aed33c0820d1eff
Bao phung <b.giaphung@gmail.com>
2021-01-02 14:43:16 -0600

    refactoring

--- project/script.js
+++ project/script.js
@@ -5,20 +5,21 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsOpenModal = document.querySelectorAll('.show-modal');
 
-console.log(btnsOpenModal);
-
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener('click', function () {
-    console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+console.log(btnsOpenModal);
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 
